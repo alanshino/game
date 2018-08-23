f696b3330a378153c65660f9901d795df7c25f96
alanshino <alanwalker@gms.tku.edu.tw>
2018-08-23 19:00:41 +0800

    add Double player

--- Tictactoe.c
+++ Tictactoe.c
@@ -9,6 +9,7 @@ void startgame(char **pc);
 int wincheck(char **pc);
 int aicomputer(char **pc);
 int originalcheck(char **pc);
+void startdouble(char **pc);
 
 int times=0;
 
@@ -52,16 +53,30 @@ void print(char **pc){
 
 void menu(char **pc){
     char *ps=malloc(sizeof(char)*10);
+    char *pi=malloc(sizeof(char)*10);
     label:
     printf("Do you want to start TicTacToe game: (Y/y or N/n): ");
     scanf("%s",ps);
     if (*ps=='Y'||*ps=='y'){
-        startgame(pc);
+        label1:
+        printf("Double play or Single match [(D/d or 2 to Double play),(S/s or 1 to Single match)]: ");
+        scanf("%s",pi);
+        if (*pi=='D'||*pi=='d'||*pi=='2'){
+            startdouble(pc);
+        }else if (*pi=='S'||*pi=='s'||*ps=='1'){
+            startgame(pc);
+        }else{
+            goto label1;
+        }
     }else if(*ps=='N'||*ps=='n'){
         overgame();
     }else{
+        fflush(stdin);
+        printf("input is error\n");
         goto label;
     }
+    free(ps);
+    free(pi);
 }
 
 void overgame(void){
@@ -85,8 +100,10 @@ void startgame(char **pc){
             *(*(panswer+i)+j)=0;
         }
     }
+    system("CLS");
+    printf("start Single match\n");
     label:
-    printf("How You want to attack first or later [ O is first ],[ X is later ]: ");
+    printf("How you want to attack first or later [ O is first ],[ X is later ]: ");
     fflush(stdin);
     scanf("%c%c",&c,&check);
     if (c=='O'||c=='o'){
@@ -258,4 +275,145 @@ int aicomputer(char **pc){
     }else{
         return 0;
     }
+}
+
+void startdouble(char **pc){
+    int flat=9;
+    char *ps=malloc(sizeof(char)*10);
+    char c,check;
+    int **panswer=malloc(sizeof(int *)*3);
+    int *pi=malloc(sizeof(int)*2);
+    for (int i=0;i<3;i++){
+        *(panswer+i)=malloc(sizeof(int)*3);
+    }
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            *(*(panswer+i)+j)=0;
+        }
+    }
+    system("CLS");
+    printf("start double player\n");
+    label:
+    printf("What you want to attack first is 'O' or 'X' : ");
+    fflush(stdin);
+    scanf("%c%c",&c,&check);
+    if (c=='O'||c=='o'){
+        label2:
+        printf("Player one time\n");
+        printf("input row and col: ");
+        scanf("%d%d",pi+0,pi+1);
+        if (*(pi+0)>3||*(pi+0)<0||*(pi+1)>3||*(pi+1)<0){
+            printf("input is error\n");
+            goto label2;
+        }else{
+            if (*(*(panswer+*(pi+0)-1)+*(pi+1)-1)==0){
+                *(*(panswer+*(pi+0)-1)+*(pi+1)-1)=1;
+                *(*(pc+*(pi+0)-1)+*(pi+1)-1)='O';
+                print(pc);
+                printf("Change player second operation\n");
+                label3:
+                printf("input row and col: ");
+                scanf("%d%d",pi+0,pi+1);
+                if (*(pi+0)>3||*(pi+0)<0||*(pi+1)>3||*(pi+1)<0){
+                    printf("input is error\n");
+                    goto label3;
+                }else{
+                    if (*(*(panswer+*(pi+0)-1)+*(pi+1)-1)==0){
+                        *(*(panswer+*(pi+0)-1)+*(pi+1)-1)=1;
+                        *(*(pc+*(pi+0)-1)+*(pi+1)-1)='X';
+                        print(pc);
+                    }else{
+                        printf("the block is already put a car\n");
+                        goto label3;
+                    }
+                }
+                if (wincheck(pc)){
+                    printf("Player one wins\n");
+                }else if (aicomputer(pc)){
+                    printf("Player two wins\n");
+                }else{
+                    for (int i=0;i<3;i++){
+                        for (int j=0;j<3;j++){
+                            if (*(*(panswer+i)+j)!=0){
+                                flat--;
+                            }
+                        }
+                    }
+                    if (flat>0){
+                        goto label2;
+                    }else{
+                        printf("Tic Tac Toe is over ,This game is tied\n");
+                        exit(1);
+                    }
+                }
+            }else{
+                printf("the block is already put a car\n");
+                goto label2;
+            }
+        }
+    }else if (c=='X'||c=='x'){
+        label4:
+        printf("Player one time\n");
+        printf("input row and col: ");
+        scanf("%d%d",pi+0,pi+1);
+        if (*(pi+0)>3||*(pi+0)<0||*(pi+1)>3||*(pi+1)<0){
+            printf("input is error\n");
+            goto label4;
+        }else{
+            if (*(*(panswer+*(pi+0)-1)+*(pi+1)-1)==0){
+                *(*(panswer+*(pi+0)-1)+*(pi+1)-1)=1;
+                *(*(pc+*(pi+0)-1)+*(pi+1)-1)='X';
+                print(pc);
+                printf("Change player second operation\n");
+                label5:
+                printf("input row and col: ");
+                scanf("%d%d",pi+0,pi+1);
+                if (*(pi+0)>3||*(pi+0)<0||*(pi+1)>3||*(pi+1)<0){
+                    printf("input is error\n");
+                    goto label5;
+                }else{
+                    if (*(*(panswer+*(pi+0)-1)+*(pi+1)-1)==0){
+                        *(*(panswer+*(pi+0)-1)+*(pi+1)-1)=1;
+                        *(*(pc+*(pi+0)-1)+*(pi+1)-1)='O';
+                        print(pc);
+                    }else{
+                        printf("the block is already put a car\n");
+                        goto label5;
+                    }
+                }
+                if (wincheck(pc)){
+                    printf("Player one wins\n");
+                }else if (aicomputer(pc)){
+                    printf("Player two wins\n");
+                }else{
+                    for (int i=0;i<3;i++){
+                        for (int j=0;j<3;j++){
+                            if (*(*(panswer+i)+j)!=0){
+                                flat--;
+                            }
+                        }
+                    }
+                    if (flat>0){
+                        goto label4;
+                    }else{
+                        printf("Tic Tac Toe is over ,This game is tied\n");
+                        exit(1);
+                    }
+                }
+            }else{
+                printf("the block is already put a car\n");
+                goto label4;
+            }
+        }
+    }else{
+        printf("input is error\n");
+        goto label;
+    }
+    for (int i=0;i<3;i++){
+        free(*(panswer+i));
+    }
+    free(panswer);
+    free(pi);
+    free(ps);
+    return ;
 }
