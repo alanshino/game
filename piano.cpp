eca896e7908bac99b763d21721f5ca09c6083f0e
alanshino <alanwalker@gms.tku.edu.tw>
2018-10-09 23:51:23 +0800

    new

---
+++ piano.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main(int argc,char **argv){
+    cout<<"Do ";
+    _beep(262,500);
+    cout<<"Re ";
+    _beep(293,500);
+    cout<<"Mi ";
+    _beep(330,500);
+    cout<<"Do ";
+    _beep(262,500);
+    cout<<"So ";
+    _beep(392,1000);
+    cout<<"Mi ";
+    _beep(330,500);
+    cout<<"Re ";
+    _beep(293,500);
+    cout<<"So ";
+    _beep(392,500);
+    cout<<"Re ";
+    _beep(293,500);
+    cout<<"Do ";
+    _beep(262,500);
+    cout<<"La ";
+    _beep(220,500);
+    cout<<"Mi ";
+    _beep(330,1000);
+    cout<<"Do ";
+    _beep(262,500);
+    cout<<"Si ";
+    _beep(247,750);
+    cout<<"Si ";
+    _beep(247,500);
+    cout<<"La ";
+    _beep(220,500);
+    cout<<"La ";
+    _beep(220,250);
+    cout<<"Si ";
+    _beep(247,500);
+    cout<<"Do ";
+    _beep(262,500);
+    cout<<"Re ";
+    _beep(293,500);
+    cout<<"So ";
+    _beep(196,500);
+    cout<<"Do ";
+    _beep(262,500);
+    cout<<"Re ";
+    _beep(293,500);
+    cout<<"Mi ";
+    _beep(330,500);
+    cout<<"Fa ";
+    _beep(349,500);
+    cout<<"Fa ";
+    _beep(349,350);
+    cout<<"Mi ";
+    _beep(330,500);
+    cout<<"Re ";
+    _beep(293,500);
+    cout<<"Do ";
+    _beep(262,500);
+    cout<<"Do ";
+    _beep(262,500);
+    return 0;
+}
